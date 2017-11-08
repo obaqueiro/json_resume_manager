febb6315ca7ac3c8877d5d7bfa59f6bd519f53b3
Omar Baqueiro <obaqueiro@OmarBaqueiroMBP.local>
2017-11-08 12:14:02 -0600

    Add initial JSON resume validation logic

--- app.rb
+++ app.rb
@@ -14,6 +14,18 @@ get '/resume' do
 end
 
 post '/resume' do
+ begin 
+  json = JSON.parse(request.body.read.to_s)
+ rescue => ex
+   return "Invalid JSON detected in body, try again.\n"
+ end
+ missing = ['basics','basics.name', 'basics.email', 'basics.work']
+ 
+ missing.dup.each { |x| missing.delete(x) if json.dig(*x.split('.')) }
+
+ if missing.length > 0 
+   return "Missing keys in JSON:  #{missing.join(',')}"
+ end
  # validate JSON body
  # Validate required fields
  # Save JSON data in databaase
